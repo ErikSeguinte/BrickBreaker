bcadf7e1950747189a43048c86fcdec912e51d50
Erik Seguinte <ErikSeguinte@gmail.com>
2018-03-23 18:54:25 -0700

    initial commit

---
+++ src/main/java/com/erikseguinte/brickBreaker/brickBreakerApp.java
@@ -0,0 +1,17 @@
+package com.erikseguinte.brickBreaker;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.settings.GameSettings;
+
+public class brickBreakerApp extends GameApplication {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+
+    }
+}

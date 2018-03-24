8403f943a2b01aa4b09e96becb1161f1d1bcb33b
Erik Seguinte <ErikSeguinte@gmail.com>
2018-03-24 14:38:07 -0700

    Rename BatControl

--- src/main/java/com/erikseguinte/brickBreaker/control/BatControl.java
+++ src/main/java/com/erikseguinte/brickBreaker/control/BatControl.java
@@ -3,7 +3,7 @@ package com.erikseguinte.brickBreaker.control;
 import com.almasb.fxgl.entity.Control;
 import com.almasb.fxgl.entity.Entity;
 
-public class batControl extends Control{
+public class BatControl extends Control{
     @Override
     public void onUpdate(Entity entity, double tpf) {
 

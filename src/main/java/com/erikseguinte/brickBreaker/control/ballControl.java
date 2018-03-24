b86474e4bff5623d49da500cc66f73aa64b41272
Erik Seguinte <ErikSeguinte@gmail.com>
2018-03-24 14:15:02 -0700

    Control Classes

---
+++ src/main/java/com/erikseguinte/brickBreaker/control/ballControl.java
@@ -0,0 +1,4 @@
+package com.erikseguinte.brickBreaker.control;
+
+public class ballControl {
+}
--- src/main/java/com/erikseguinte/brickBreaker/control/batControl.java
+++ src/main/java/com/erikseguinte/brickBreaker/control/batControl.java
@@ -1,4 +1,11 @@
 package com.erikseguinte.brickBreaker.control;
 
-public class batControl {
+import com.almasb.fxgl.entity.Control;
+import com.almasb.fxgl.entity.Entity;
+
+public class batControl extends Control{
+    @Override
+    public void onUpdate(Entity entity, double tpf) {
+
+    }
 }

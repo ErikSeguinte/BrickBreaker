0cf6a430baa91fc8ed0128d7fac14460321bc904
Erik Seguinte <ErikSeguinte@gmail.com>
2018-03-25 17:57:43 -0700

    Initial commit of Brick Control

---
+++ src/main/java/com/erikseguinte/brickBreaker/control/BrickControl.java
@@ -0,0 +1,11 @@
+package com.erikseguinte.brickBreaker.control;
+
+import com.almasb.fxgl.entity.Control;
+import com.almasb.fxgl.entity.Entity;
+
+public class BrickControl extends Control {
+    @Override
+    public void onUpdate(Entity entity, double tpf) {
+
+    }
+}
